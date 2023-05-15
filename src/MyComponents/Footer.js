52b34db8e04ea19ab6cab0fbf1cc5a94b4b9a6c6
chandresh2727 <trivedichandresh882@gmail.com>
2023-05-15 18:48:35 +0530

    ToDoS-LiSt

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Footer = () => {
+  // let footerStyle = {
+  //   position: "relative",
+  //   top:"10vh",
+  //   width:"100%"
+  // }
+
+  return (
+    // <footer className="bg-dark text-light" style={footerStyle}>
+    <footer className="bg-dark text-light">
+      <p className="text-center">
+      Copyright &copy; MyTodos list
+      </p>
+    </footer>
+  )
+}
+
+// export default Footer
